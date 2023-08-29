4c83bdbd0cbe2d305bd3e4dc569a7c1629f600b6
Alfred Peter <alpeter91@gmail.com>
2023-08-29 22:48:41 +0300

    Add species column

--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 	name VARCHAR(255),
 	date_of_birth DATE,
 	escape_attempts INT,
 	neutered BOOL,
-	weight_kg FLOAT
+	weight_kg FLOAT,
+	species VARCHAR(255)
 );
