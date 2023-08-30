066fbf6575b97b57b27df24a8c91a4b49457bef7
Alfred Peter <alpeter91@gmail.com>
2023-08-30 22:37:56 +0300

    Update animals table to include species info

--- data.sql
+++ data.sql
@@ -17,3 +17,9 @@ INSERT INTO species (name) VALUES
 ('Pokemon'),
 ('Digimon');
 
+UPDATE animals
+SET species_id = CASE
+	WHEN animals.name LIKE '%mon' THEN (SELECT id FROM species WHERE name = 'Digimon')
+    ELSE (SELECT id FROM species WHERE name = 'Pokemon')
+    END;
+
