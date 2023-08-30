d5da93fcbfd6dc2f9a44dd250dec381799f55f43
Alfred Peter <alpeter91@gmail.com>
2023-08-30 22:38:21 +0300

    Populate owners table

--- data.sql
+++ data.sql
@@ -23,3 +23,10 @@ SET species_id = CASE
     ELSE (SELECT id FROM species WHERE name = 'Pokemon')
     END;
 
+INSERT INTO owners (full_name, age) VALUES
+('Sam Smith', 34),
+('Jeniffer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
