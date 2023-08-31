17a982454b0967b68aaa5be46ccdf07c2e06e1d3
Alfred Peter <alpeter91@gmail.com>
2023-08-31 15:28:52 +0300

    Populate vets table

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
+-- Populate animals table
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES
 ('Agumon', '2020-02-03', 0, TRUE, 10.23),
 ('Gabumon', '2018-11-15', 2, TRUE, 8.00),
@@ -13,16 +14,19 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 ('Blossom', '1998-10-13', 3, TRUE, 17),
 ('Ditto', '2022-05-14', 4, TRUE, 22);
 
+-- Populate species table
 INSERT INTO species (name) VALUES
 ('Pokemon'),
 ('Digimon');
 
+-- Updade species column in animals table
 UPDATE animals
 SET species_id = CASE
 	WHEN animals.name LIKE '%mon' THEN (SELECT id FROM species WHERE name = 'Digimon')
     ELSE (SELECT id FROM species WHERE name = 'Pokemon')
     END;
 
+-- Populate owners table
 INSERT INTO owners (full_name, age) VALUES
 ('Sam Smith', 34),
 ('Jeniffer Orwell', 19),
@@ -31,6 +35,7 @@ INSERT INTO owners (full_name, age) VALUES
 ('Dean Winchester', 14),
 ('Jodie Whittaker', 38);
 
+-- Update animals table
 UPDATE animals
 SET owners_id = CASE
 	WHEN animals.name = 'Agumon'
@@ -44,3 +49,10 @@ SET owners_id = CASE
 	WHEN animals.name IN ('Angemon', 'Boarmon')
 		THEN (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
     END;
+
+-- Populate vets table
+INSERT INTO vets (name, age, date_of_graduation) VALUES
+('Vet William Tatcher', 45, '2000-04-23'),
+('Vet Maisy Smith', 26, '2019-01-17'),
+('Vet Stephanie Mendez', 64, '1981-05-04'),
+('Vet Jack Harkness', 38, '2008-06-08');
